cacc415be1008655330bce168083afb9d958a0ba
bang9859 <kokozz9859@gmail.com>
2024-11-08 15:50:11 +0900

    Feat constructor and Add variable

--- src/starcraft/Unit.java
+++ src/starcraft/Unit.java
@@ -1,5 +1,13 @@
 package starcraft;
 
-public class Unit {
-
+abstract public class Unit {
+	final int MAX_HP;
+	int hp;
+	String name;
+	Unit (String name,int hp){
+		MAX_HP = hp;
+		this.name = name;
+		this.hp = hp;
+	}
+	
 }
