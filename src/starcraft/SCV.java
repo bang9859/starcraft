3dff309a258768ae10493a77e62af51b37ebda08
bang9859 <kokozz9859@gmail.com>
2024-11-08 16:10:31 +0900

    Feat repair method

--- src/starcraft/SCV.java
+++ src/starcraft/SCV.java
@@ -5,5 +5,20 @@ public class SCV extends GroundUnit implements Repairable{
 	public SCV(String name,int hp){
 		super(name,hp);
 	}
-
+	
+	public void repair(Repairable unit) {
+		if(unit instanceof Unit) {
+			Unit target = (Unit)unit;
+			while(target.hp>target.MAX_HP) {
+				String msg = String.format("%s [%d/%d] 수리중...", target.name,target.hp++,target.MAX_HP);
+				System.out.println(msg);
+				try {
+					Thread.sleep(300);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.printf("%s 수리완료!\n",target.name);
+		}
+	}
 }
