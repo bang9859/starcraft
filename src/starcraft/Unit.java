92601458ac4f68987222c7ad8604c5b4f0d4a795
bang9859 <kokozz9859@gmail.com>
2024-11-11 12:16:22 +0900

    Update variable modifier

--- src/starcraft/Unit.java
+++ src/starcraft/Unit.java
@@ -2,8 +2,8 @@ package starcraft;
 
 abstract public class Unit {
 	protected final int MAX_HP;
-	int hp;
-	String name;
+	protected int hp;
+	protected String name;
 	protected Unit (String name,int hp){
 		MAX_HP = hp;
 		this.name = name;
